32a4dbd145e834fd985598b489f4afa07ec37cac
nieveslnormanss@gmail.com <>
2023-06-02 15:41:07 +0800

    1

--- Assets/NakamaClient.cs
+++ Assets/NakamaClient.cs
@@ -2,13 +2,12 @@ using System.Collections.Generic;
 using UnityEngine;
 using Nakama;
 using Nakama.TinyJson;
+using System.Collections;
 
 public class NakamaClient : MonoBehaviour
 {
-    
     private const string RoomName = "heroes";
     private readonly IClient client = new Client("http","192.168.71.167",7350,"defaultkey");
-
     private ISocket socket;
     async void Start()
     {
@@ -23,6 +22,23 @@ public class NakamaClient : MonoBehaviour
 
         socket.Closed +=() => Debug.Log("Socket closed");
 
+        var enc = System.Text.Encoding.UTF8;
+        socket.ReceivedMatchState += newState =>
+        {
+            var content = enc.GetString(newState.State);
+
+            switch (newState.OpCode)
+            {
+                case 1:
+                    Debug.Log("A custom opcode.");
+                    Debug.Log(content);
+                    break;
+                default:
+                    Debug.Log("default");
+                    break;
+            }
+        };
+
         await socket.ConnectAsync(session);
 
         string rpcId = "rpc_create_match";
@@ -30,7 +46,25 @@ public class NakamaClient : MonoBehaviour
         var response = await socket.RpcAsync(rpcId);
 
         Debug.Log("RPC Response: " + response.Payload);
-        
+
+        var newState = new Dictionary<string, string> {{"hello", "world"}}.ToJson();
+
+        //await socket.SendMatchStateAsync(response.Payload,1,newState);
+
+        StartCoroutine(ExecuteEverySecond(response.Payload,newState));
+
+    }
+
+    private IEnumerator ExecuteEverySecond(string matchID,string newstate)
+    {
+        while (true)
+        {
+            // 执行需要每秒执行的逻辑
+            Debug.Log("Repeated execution every 1 sec");
+            socket.SendMatchStateAsync(matchID,1,newstate);
+
+            yield return new WaitForSeconds(1f);
+        }
     }
 
     // Update is called once per frame
